da2b25310ea9ac7b8cdd211f428d2509072d358f
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-08-07 15:09:43 +0200

    feat: generate grid dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
 import './App.css';
 import Counter from './components/Counter';
+import Grid from './components/Grid';
 import SingleLight from './components/SingleLight';
 import Text from './components/Text';
+import { useState } from 'react';
 
 function App() {
+	const [rows, setRows] = useState(0);
+	const [columns, setColumns] = useState(0);
+
+	const increment = (setElement, el) => {
+		switch (el) {
+			case 'columns':
+				setElement((prev) => (prev >= 9 ? prev : prev + 1));
+				break;
+			default:
+				setElement((prev) => prev + 1);
+		}
+	};
+
+	const decrement = (setElement) => {
+		setElement((prev) => (prev <= 0 ? prev : prev - 1));
+	};
+
+	const totalLights = rows * columns;
+
+	const lights = Array.from({ length: totalLights });
+
 	return (
 		<div>
 			<nav className='nav'>
-				<Text className='counter'>0 bulbs</Text>
-				<Counter text='rows' count={0} />
-				<Counter text='columns' count={0} />
+				<Text className='counter'>{totalLights} bulbs</Text>
+				<Counter
+					text='rows'
+					count={rows}
+					onIncrement={() => increment(setRows)}
+					onDecrement={() => decrement(setRows)}
+				/>
+				<Counter
+					text='columns'
+					count={columns}
+					onIncrement={() => increment(setColumns, 'columns')}
+					onDecrement={() => decrement(setColumns)}
+				/>
 			</nav>
-			<div>
-				<SingleLight />
-			</div>
+			<Grid columns={columns}>
+				{lights.map((light, index) => {
+					return <SingleLight key={index} />;
+				})}
+			</Grid>
 		</div>
 	);
 }
---
+++ src/components/Grid.jsx
@@ -0,0 +1,12 @@
+﻿const styles = (columns) => {
+	return {
+		display: 'grid',
+		gridTemplateColumns: `repeat(${columns}, 109px)`,
+		columnGap: '20px',
+		rowGap: '20px',
+	};
+};
+
+export default function Grid(props) {
+	return <div style={styles(props.columns)}>{props.children}</div>;
+}
